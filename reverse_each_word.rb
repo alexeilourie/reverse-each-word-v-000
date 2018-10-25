3f03d54025ad1bbf5f9e0323bb33505a255b0dfc
Alexei Lourie <alexeilourie@gmail.com>
2018-10-25 17:03:35 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,11 @@
 def reverse_each_word(word)
   arr = word.split(" ")
   reversed_word = ""
-  arr.each do |word|
-    counter = word.length
-    while word.length > 0
+  arr.each do |single_word|
+    counter = single_word.length
+    while single_word.length > 0
        counter -= 1
-       reversed_word << word[counter]
+       reversed_word << single_word[counter]
     end
     reversed_word << " "
   end
